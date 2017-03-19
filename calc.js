16cd63297deb5a402e2f0ce8404a6b0432a98d7c
Viachaslau Karzhou <punicorn48@gmail.com>
2017-03-19 14:55:36 +0300

    5-th function was added. Usage examples.

--- calc.js
+++ calc.js
@@ -1,15 +1,32 @@
 function sum(a, b) {
-	return a + b;
+    return a + b;
 }
 
 function subtract(a, b) {
-	return a - b;
+    return a - b;
 }
 
 function multiply(a, b) {
-	return a * b;
+    return a * b;
 }
 
 function divide(a, b) {
-	return a / b;
+    return a / b;
 }
+
+function divInt(a, b) {
+    return (a - a % b) / b;
+}
+
+console.log(divInt(7, 2));
+console.log(divInt(-7, 2));
+console.log(divide(-7, 2));
+console.log(divide(false, 2));
+console.log(multiply(2.12, 10));
+console.log(multiply(2.12, 10).toFixed(1));
+console.log(multiply(true, 10));
+console.log(subtract(3, 2));
+console.log(subtract(3, 'false'));
+console.log(sum(undefined, 2));
+console.log(sum(null, 2));
+console.log(sum(48, 2));
