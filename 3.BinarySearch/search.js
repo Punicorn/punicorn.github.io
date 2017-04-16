0a394fbcfc7ae166930b0bd4fdf12f82569446f2
Viachaslau Karzhou <punicorn48@gmail.com>
2017-04-16 15:47:21 +0300

    optimized return

--- 3.BinarySearch/search.js
+++ 3.BinarySearch/search.js
@@ -2,20 +2,17 @@ function binarySearch(arr, target) {
   var half = 0;
   var left = 0;
   var right = arr.length - 1;
-  while(left <= right && target != arr[half]) {
+  while(left <= right) {
     half = Math.floor((right + left) / 2);
     if(target < arr[half]) {
       right = half - 1;
     } else if(target > arr[half]) {
       left = half + 1;
     }
+    if(target == arr[half]) return half;
   }
 
-  if(target == arr[half]) {
-    return half;
-  } else {
-    return -1;
-  }
+  return -1;
 }
 
 console.log(binarySearch([3,5,6,8,10,13], 13)); //5-th
